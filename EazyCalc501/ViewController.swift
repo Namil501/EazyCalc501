b80d38931fa281400d2b7bb03e5c45cf4bd4cade
namil501 <0.wave73.1@gmail.com>
2018-09-21 03:01:06 +0900

    Initial Commit

---
+++ EazyCalc501/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EazyCalc501
+//
+//  Created by MB501 on 2018/09/21.
+//  Copyright © 2018年 Niwatory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
